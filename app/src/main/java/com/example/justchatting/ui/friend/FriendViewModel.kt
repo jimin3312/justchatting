db5656af839b8ca777eb979422840f9b6585eec1
jimin <jimin003312@gmail.com>
2020-08-18 16:09:50 +0900

    fix friend_room

--- app/src/main/java/com/example/justchatting/ui/friend/FriendViewModel.kt
+++ app/src/main/java/com/example/justchatting/ui/friend/FriendViewModel.kt
@@ -5,6 +5,11 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import com.example.justchatting.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 
@@ -19,51 +24,15 @@ class FriendViewModel(application: Application) : AndroidViewModel(application),
 
     fun getUsers() {
         users = userRepository.load(getApplication())
-
-    fun setListener()
-    {
-        val uId = FirebaseAuth.getInstance().uid
-        val ref = FirebaseDatabase.getInstance().getReference("/friends/$uId")
-
-        ref.addChildEventListener(object : ChildEventListener{
-            override fun onCancelled(error: DatabaseError) {
-            }
-            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
-
-            }
-            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
-                val user = snapshot.getValue(User::class.java) ?: return
-
-//                usersMutableLiveData.value?.forEach {
-//                    Log.d("FriendFragment", "onChildChanged : ${it.username}")
-//                    arrayList.add(it)
-//                }
-                Log.d("FriendFragment", "onChildChanged : ${user.username}")
-                arrayList.add(user)
-                usersMutableLiveData.postValue(arrayList)
-            }
-            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                val user = snapshot.getValue(User::class.java) ?: return
-
-//                usersMutableLiveData.value?.forEach {
-//                    Log.d("FriendFragment", "onChildAdded : ${it.username}")
-//                    arrayList.add(it)
-//                }
-                Log.d("FriendFragment", "onChildAdded : ${user.username}")
-                arrayList.add(user)
-                usersMutableLiveData.postValue(arrayList)
-            }
-            override fun onChildRemoved(snapshot: DataSnapshot) {
-            }
-        })
     }
 
     fun sync() {
-        Log.d("FriendViewModel","sync start")
+        Log.d("FriendViewModel", "sync start")
         userRepository.sync(getApplication())
         users = userRepository.load(getApplication())
         myUser = userRepository.getMyUser(getApplication())
     }
+
 //    fun erase()
 //    {
 //        var eraseCount : Long = 0
