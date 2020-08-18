c1d69fdba939c7bb54ee6775bafe4bb71681e9a2
YTH <2354taeng@gmail.com>
2020-08-18 11:44:58 +0900

    signUp, include exception

--- app/src/main/java/com/example/justchatting/User.kt
+++ app/src/main/java/com/example/justchatting/User.kt
@@ -1,5 +1,8 @@
 package com.example.justchatting
 
-class User(val uid: String, val username : String, val phoneNumber: String, val profileImageUrl : String?) {
-    constructor(): this("","","","")
-}
+data class User(
+    val uid: String,
+    val username: String,
+    val phoneNumber: String,
+    val profileImageUrl: String?
+)
