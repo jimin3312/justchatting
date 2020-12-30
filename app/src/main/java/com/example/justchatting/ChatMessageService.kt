6499e9d858f6f9dc2352ae40bbfc7e21c3845022
YTH <2354taeng@gmail.com>
2020-12-30 22:49:19 +0900

    update toekn

--- app/src/main/java/com/example/justchatting/ChatMessageService.kt
+++ app/src/main/java/com/example/justchatting/ChatMessageService.kt
@@ -5,21 +5,27 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.media.RingtoneManager
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import com.example.justchatting.repository.login.UserRepository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import org.koin.android.ext.android.inject
 import kotlin.random.Random
 
-class ChatMessageService : FirebaseMessagingService() {
+class ChatMessageService() : FirebaseMessagingService() {
+
+    val repository: UserRepository by inject()
 
     companion object{
         val CHANNEL_ID = "1234"
     }
+
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         Log.d("메세지", "From: ${message.from}")
@@ -67,9 +73,15 @@ class ChatMessageService : FirebaseMessagingService() {
     }
     override fun onNewToken(token: String) {
         Log.d("새 토큰", token)
-        val uid = FirebaseAuth.getInstance().uid
-        if (uid != null) {
-            FirebaseDatabase.getInstance().getReference("/users/$uid").child("token").setValue(token)
+
+        if(FirebaseAuth.getInstance().uid != null) {
+            repository.updateToken()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                }, {
+                    // updating error handle
+                })
         }
     }
 }
